1bb11f00b2214c2283a9ebdb4b6f9478b39ee0aa
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2022-02-01 20:57:04 +0530

    Fix grammar

--- docs/tools/touchbar.sh
+++ docs/tools/touchbar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022 Aditya Garg
 
-echo "Setting up the touchbar"
+echo "Setting up the Touch Bar"
 echo -e "# display f* key in touchbar\noptions apple-ib-tb fnmode=1" | sudo tee /etc/modprobe.d/apple-tb.conf >/dev/null
 echo -e "# blacklist the touchbar driver for delayed loading\nblacklist apple_ib_tb" | sudo tee /etc/modprobe.d/blacklist-tb.conf >/dev/null
 echo -e "#!/usr/bin/env bash\n\nmodprobe apple-ib-tb" | sudo tee /usr/local/bin/tb-load.sh >/dev/null
@@ -9,7 +9,7 @@ echo -e "#!/usr/bin/env bash\n\nmodprobe apple-ib-tb" | sudo tee /usr/local/bin/
 echo '
 #!/usr/bin/env bash
 
-echo "Select touchbar mode"
+echo "Select Touch Bar mode"
 echo
 echo "0: Only show F1-F12"
 echo "1: Show media and brightness controls, use the fn key to switch to F1-12"
@@ -39,5 +39,5 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl enable touchbar.service
-echo "Run sudo touchbar to set touchbar modes in subsequent boots"
-echo "Reboot to finish setting up of touchbar"
+echo "Run sudo touchbar to change default Touch Bar mode in subsequent boots"
+echo "Reboot to finish setting up Touch Bar"
