28067c10dca1f939dd786eb842070fdb438bfbe9
Joshua Goins <josh@redstrate.com>
2022-04-06 14:20:13 -0400

    Fix rmmod_tb.sh
    This fixes sleep/resume on Arch Linux, otherwise the
    keyboard and touchbar fail to wake up properly.

--- docs/tools/rmmod_tb.sh
+++ docs/tools/rmmod_tb.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ "${1}" = "pre" ]; then
         modprobe -r apple_ib_tb
+        modprobe -r hid_apple
 elif [ "${1}" = "post" ]; then
+        modprobe hid_apple
         modprobe apple_ib_tb
 fi
